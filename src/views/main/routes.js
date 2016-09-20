4dd208f34873eb8edcf965a625571cff7be32b51
Megan Coyle <meg@mcoyle.com>
2016-09-20 16:34:10 -0400

    Mapping and Yelp logic

--- src/views/main/routes.js
+++ src/views/main/routes.js
@@ -1,21 +1,19 @@
 import React from 'react'
 import {Route, IndexRoute} from 'react-router'
-
 import Container from './Container'
-import IndexPage from './indexPage/IndexPage'
+
+import Map from './Map/Map'
+import Detail from './Detail/Detail'
 
 export const makeMainRoutes = () => {
   return (
     <Route path="/" component={Container}>
-      {/* Lazy-loading */}
-      <Route path="about" getComponent={(location, cb) => {
-          require.ensure([], (require) => {
-            const mod = require('./about/About');
-            cb(null, mod.default);
-          });
-        }} />
-      {/* inline loading */}
-      <IndexRoute component={IndexPage} />
+      <Route path="map" component={Map}>
+      <Route path="detail/:placeId"
+        component={Detail} />
+      </Route>
+
+      <IndexRoute component={Map} />
     </Route>
   )
 }
